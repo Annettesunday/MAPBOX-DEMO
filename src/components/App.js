cdd4ff955360cd5a7e714a06873a85331f75343b
annettesunday <annette.odhiambo@andela.com>
2019-10-17 17:08:23 +0300

    ch(src): configure react-map-gl and display interactive map

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import * as parkData from "../data/skateboard-parks.json";
 
-function App() {
-  return <div>map</div>;
-}
+const App = () => {
+  const [viewport, setViewport] = useState({
+    latitude: 45.4211,
+    longitude: -75.6903,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10
+  });
 
+  const [selectedPark, setSelectedPark] = useState(null);
+
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/annettesunday/ck1uq7m3r0cwz1cojg3pt4kvl"
+        onViewportChange={viewport => setViewport(viewport)}
+      >
+        {parkData.features.map(park => {
+          return (
+            <Marker
+              key={park.properties.PARK_ID}
+              latitude={park.geometry.coordinates[1]}
+              longitude={park.geometry.coordinates[0]}
+            >
+              <button
+                className="marker-btn"
+                onClick={e => {
+                  e.preventDefault();
+                  setSelectedPark(park);
+                }}
+              >
+                <img src="/skateboarding.svg" alt="Skate Parl Icon" />
+              </button>
+            </Marker>
+          );
+        })}
+        {selectedPark && (
+          <Popup
+            onClose={() => {
+              setSelectedPark(null);
+            }}
+            latitude={selectedPark.geometry.coordinates[1]}
+            longitude={selectedPark.geometry.coordinates[0]}
+          >
+            <h2>{selectedPark.properties.NAME}</h2>
+            <p>{selectedPark.properties.DESCRIPTIO}</p>
+          </Popup>
+        )}
+      </ReactMapGL>
+    </div>
+  );
+};
 export default App;
